5f46d902dc53f1481c883a2f55ee62ffa5e771dd
HarpinderJotSingh <harpinderjot36@gmail.com>
2019-09-20 09:41:32 +0530

    Almost breaked ( vigenere plus bruteforce analysis )

--- assignment1/Crack.py
+++ assignment1/Crack.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+import string
 from BitVector import *                                                       
+from collections import Counter
 
 if len(sys.argv) is not 3:                                                    
     sys.exit('''Needs two command-line arguments, one for '''
@@ -13,12 +15,22 @@ PassPhrase = "I want to learn cryptograph and network security"
 BLOCKSIZE = 64                                                               
 numbytes = BLOCKSIZE // 8     
 
-def binaryToAscii(binary):
+def binToAsciiStr(binary):
     final_msg = ''
     for i in range(0, len(binary), 8):
         block = binary[i:i+8]
-        final_msg+=chr(int(str(block),2))	                                      
+        final_msg+=chr(int(str(block),2))
     return final_msg
+
+
+def binaryToAscii(binary):
+    # msg = chr(int(str(binary),2))
+    k = BitVector( bitstring = binary ) ^ BitVector( bitstring = bin(ord('e'))[2:] )
+    return chr(int(str(k),2))
+
+# print(binaryToAscii(bin(ord('h'))))
+# print()
+# print()
 
 def hexToBinary(hex):
     binary = ""
@@ -27,7 +39,32 @@ def hexToBinary(hex):
         char = bin(int(hex[i], scale))[2:].zfill(numbytes//2)   			# for each hex character make it a 4 bit binary
         binary+=char
     return binary
-                                               
+
+def secondMax(my_list):
+    # print(my_list)
+    freq = {} 
+    for item in my_list: 
+        if (item in freq): 
+            freq[item] += 1
+        else: 
+            freq[item] = 1
+    # print(freq)
+    max1 = 0
+    ans1 = -1
+    for key, value in freq.items():
+        if (value > max1):
+            ans1 = key
+    del freq[key]
+    # print(freq)
+    max2 = 0
+    ans2 = -1
+    for key, value in freq.items():
+        if (value > max2):
+            ans2 = key
+    if(ans2 != -1):
+        return ans2
+    else:
+        return ans1
 #Open encrypted text file and read it
 cipherFile = open(sys.argv[1],'r')
 cipherText = cipherFile.read()
@@ -61,18 +98,49 @@ for i in range(len(cipherBlocks)-1,0,-1):
     pxorkBlocks.insert(0,pxork)
 
 
-numBlocks = BLOCKSIZE//8
+numRows = BLOCKSIZE//8
 charSize = 8
-vigCArray = [[] for y in range(numBlocks)]
+vigCArray = [[] for y in range(numRows)]
 #vigCArray is a 2d matrix where each row represents a addition cipher
 
-for i in range(0,len(pxorkBlocks)):
-    pxorkStr = str(pxorkBlocks[i])
+# print(pxorkBlocks[0]^pxorkBlocks[1]^pxorkBlocks[2])
+
+for i in range(0,len( pxorkBlocks )):
+    pxorkStr = str( pxorkBlocks[i] )
+    # print(pxorkStr)
     count = 0
     for j in range(0, len(pxorkStr),charSize):
-        vigCArray[count].append(pxorkStr[j:j+charSize])
+        temp = pxorkStr[j:j+charSize]
+        vigCArray[count].append(temp)
         count = count+1
 
-for i in range(8):
-    result = max(vigCArray[i], key = vigCArray[i].count)
-    print(binaryToAscii(result))
+listAscii = string.printable
+
+for i in range( len( listAscii ) ):
+    char = listAscii[i]
+    encKey = ''
+    encKey2 = ''
+    for i in range(8):
+        maxOne = max( vigCArray[i], key = vigCArray[i].count )
+        result = secondMax(vigCArray[i])
+        result2 = maxOne
+        # print(maxOne)
+        result = str( BitVector(bitstring = result) ^ BitVector( textstring = char ) )
+        result2 = str( BitVector(bitstring = result2) ^ BitVector( textstring = char ) )
+        # print( binaryToAscii( result ) )
+        encKey += result
+        encKey2 += result2
+
+    encKeyBVctr = BitVector( bitstring = encKey)
+    encKeyBVctr2 = BitVector( bitstring = encKey2)
+    # print(len(encKeyBVctr))
+    # print(cipherBlocks[0] ^ cipherBlocks[1])
+    ans1 = binToAsciiStr(pxorkBlocks[0] ^ encKeyBVctr)
+    ans2 = binToAsciiStr(pxorkBlocks[1] ^ encKeyBVctr)
+    ans12 = binToAsciiStr(pxorkBlocks[0] ^ encKeyBVctr2)
+    ans22 = binToAsciiStr(pxorkBlocks[1] ^ encKeyBVctr2)
+    # print(len(ans))
+    # print('hmm')
+    # print(ans1+ans2)
+    # print('hmm2')
+    print(ans12+ans22)
